c2770b20b6bcdf6f65a2ec6e45aedc7653f68637
Luis Gonzalez <162064187+LuisJG8@users.noreply.github.com>
2024-08-07 12:28:51 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import turtle
+import pandas
+from turtle import Turtle
+
+screen = turtle.Screen()
+screen.title("U.S. State Games")
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+tim = Turtle()
+
+def get_mouse_click_coor(x, y):
+    print(x, y)
+
+turtle.onscreenclick(get_mouse_click_coor)
+
+
+my_file = pandas.read_csv("50_states.csv")
+all_states = my_file.state
+# print(my_file.state == answer)
+
+game_on = True
+while game_on:
+
+    answer = screen.textinput(title="Guess the state", prompt="Name a state in the map").title()
+    if answer == "Exit":
+        game_on = False
+    for state in all_states:
+        if answer == state:
+            print(answer)
+            find_state_info = my_file[my_file.state == answer]
+            the_index = my_file[my_file.state == answer].index
+
+            state_to_dictionary = find_state_info.to_dict()
+            print(state_to_dictionary)
+            print("\n")
+
+            new_index = the_index[0]
+            print(new_index)
+
+            new_x = state_to_dictionary["x"][new_index]
+            new_y = state_to_dictionary["y"][new_index]
+            my_tupil = new_x, new_y
+            print(my_tupil)
+
+            tim.penup()
+            tim.goto(x=new_x, y=new_y)
+            tim.color("red")
+            tim.write(arg=f"{answer}")
+
+
+turtle.mainloop()
